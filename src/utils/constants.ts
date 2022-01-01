6efba51a9f01a6b0f5725a62b3ca638acec45734
Dineshverma <dineshverma@ip-192-168-29-157.ap-south-1.compute.internal>
2022-01-01 22:06:54 +0530

    Removed everything

--- src/utils/constants.ts
+++
@@ -1,7 +0,0 @@
-export const BLOG_AUTHOR_NAME = "Dinesh Verma"
-export const BLOG_AUTHOR_SOCIAL_HANDLES = {
-  twitter: "https://twitter.com/dineshverma",
-}
-export const BLOG_TITLE = "LadWhoCodes"
-export const BLOG_HOME_DESCRIPTION =
-  "A blog by Dinesh Verma about programming, platforms, books, music and Indian startup culture."
