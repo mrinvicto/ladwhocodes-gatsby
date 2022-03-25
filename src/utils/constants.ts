195173c7e3c5f011c5391260b054a64d1f078f34
Dineshverma <dineshverma@GL-LAP-BLR-0511.local>
2022-03-25 21:47:23 +0530

    Blog template done

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,19 +1,28 @@
 import { CatgeoryPageDetails } from "../models/CatgeoryPageDetails"
+
+// Default site properties
+export const SITE_URL = "https://ladwhocodes.com"
 export const BLOG_HOME_TITLE = "LadWhoCodes"
 export const BLOG_TITLE_SUFFIX = "LadWhoCodes"
 export const BLOG_DESCRIPTION = "LadWhoCodes in the best blog"
 export const BLOG_KEYWORDS = "LadWhoCodes, Dinesh Verma"
 export const HOMEPAGE_TITLE = "LadWhoCodes - Hello World"
 export const BLOG_LIST_PAGE_TITLE_PREFIX = "LadWhoCodes"
+
+// 404 Error page
 export const NOT_FOUND_PAGE_TITLE = "404 - Page Not Found"
 export const NOT_FOUND_DESCRIPTION =
   "Sorry, the page you are looking for is no longer available on LadWhoCodes."
-export const SITE_URL = "https://ladwhocodes.com"
+
+// Twitter social handles
 export const TWITTER_CREATOR_HANDLE = "@dineshverma"
 export const SITE_TWITTER_HANDLE = "@dineshverma"
+
+// Default OG Tag values
 export const DEFAULT_OG_SHARE_IMAGE = "https://ladwhocodes.com/"
 export const OG_ABOUT_ME_PAGE = "https://ladwhocodes.com/about"
 
+// Category page values
 export const CATEGORY_DETAILS: { [key: string]: CatgeoryPageDetails } = {
   java: {
     title: "Java",
