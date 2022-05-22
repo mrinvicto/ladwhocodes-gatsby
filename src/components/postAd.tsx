7c8e0590746f4862f6109c26de187a3ea8406162
Dineshverma <dineshverma@GL-LAP-BLR-0511.local>
2022-05-22 10:59:31 +0530

    In post ads live

--- src/components/postAd.tsx
+++ src/components/postAd.tsx
@@ -1,29 +1,24 @@
 import React, { useEffect, useRef } from "react"
 
 const InPostAd = () => {
-  const scriptContainer = useRef(null)
-
-  useEffect(() => {
-    const scriptString = `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6999295635811899"
-          crossorigin="anonymous"></script>
-      <!-- ladWhoCodes-In-Article -->
-      <ins class="adsbygoogle"
-          style="display:block"
-          data-ad-client="ca-pub-6999295635811899"
-          data-ad-slot="2206862299"
-          data-ad-format="auto"
-          data-full-width-responsive="true"></ins>
-      <script>
-          (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>`
-    scriptContainer.current.innerHTML = scriptString
-    // console.log({ scriptContainer })
-  }, [])
+  const scriptString = `<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6999295635811899"
+  crossorigin="anonymous"></script>
+<!-- ladWhoCodes-In-Article -->
+<ins class="adsbygoogle"
+  style="display:block"
+  data-ad-client="ca-pub-6999295635811899"
+  data-ad-slot="2206862299"
+  data-ad-format="auto"
+  data-full-width-responsive="true"></ins>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({});
+</script>`
 
   return (
-    <div className="post-ad-container" ref={scriptContainer}>
-      Hey I am ad
-    </div>
+    <div
+      className="post-ad-container"
+      dangerouslySetInnerHTML={{ __html: scriptString }}
+    ></div>
   )
 }
 
